ab7502bbaaa91f794ec2c0d540ea90c710fbd614
by4795 <catherine.siddle@ukaea.uk>
2024-02-09 10:03:20 +0000

    Docstrings improved

--- inflammation/models.py
+++ inflammation/models.py
@@ -1,5 +1,9 @@
 """Module containing models representing patients and their data.
 
+Functions:
+    load_csv - returns the text of a csv file
+    load_json - returns a file's contents into the 'observations' file
+
 The Model layer is responsible for the 'business logic' part of the software.
 
 Patients' data is held in an inflammation table (2D array) where each row contains 
@@ -41,16 +45,25 @@ def load_json(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :returns: An array of mean values of measurements for each day.
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :returns: An array of max values of measurements for each day.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :returns: An array of minimum values of measurements for each day."""
     return np.min(data, axis=0)
 
